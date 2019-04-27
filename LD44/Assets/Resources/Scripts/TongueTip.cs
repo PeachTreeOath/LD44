6e92280d4c133994bc632e486b53415400dd1e47
Mike Heitzmann <mikeh@fizzpow.com>
2019-04-27 16:35:50 -0600

    Tongue retracts when mouse is released

--- LD44/Assets/Resources/Scripts/TongueTip.cs
+++ LD44/Assets/Resources/Scripts/TongueTip.cs
@@ -34,6 +34,12 @@ public class TongueTip : MonoBehaviour
             if (isTongueReturning)
                 transform.position = Vector3.MoveTowards(transform.position, returnPosition, moveSpeed * Time.deltaTime);
 
+            if (Input.GetButtonUp("Fire1"))
+            {
+                isTongueExtending = false;
+                isTongueReturning = true;
+            }
+
             if (targetPosition == transform.position)
             {
                 isTongueExtending = false;
@@ -55,8 +61,8 @@ public class TongueTip : MonoBehaviour
         if (isEnemyTongued)
         {
             Debug.Log("I've tongued something, make me do something");
-            isEnemyTongued = false;
-            tonguedEnemy = null;
+            // isEnemyTongued = false;
+            // tonguedEnemy = null;
         }
     }
 
