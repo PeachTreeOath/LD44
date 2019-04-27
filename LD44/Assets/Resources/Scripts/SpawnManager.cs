3901a6169e2b3e69af792a778f09b0361ddc53ad
Ian <russmoonbase@gmail.com>
2019-04-27 17:58:04 -0600

    Update to wave Timer

--- LD44/Assets/Resources/Scripts/SpawnManager.cs
+++ LD44/Assets/Resources/Scripts/SpawnManager.cs
@@ -4,111 +4,112 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject spawnPointParent;
+   [SerializeField]
+   private GameObject spawnPointParent;
 
-    Transform[] allChildren;
+   Transform[] allChildren;
 
-    [SerializeField]
-    private GameObject[] enemyTypes;
+   [SerializeField]
+   private GameObject[] enemyTypes;
 
-    private int enemyAmountMax = 3;
-    private int enemyAmount = 0;
-    private int enemyTypeMaxRange = 0;
-    private int enemiesDead;
+   private int enemyAmountMax = 3;
+   private int enemyAmount = 0;
+   private int enemyTypeMaxRange = 0;
+   private int enemiesDead;
 
-    private int randomSpawnPointNum;
-    private int randomEnemyType;
+   private int randomSpawnPointNum;
+   private int randomEnemyType;
 
-    [SerializeField]
-    private bool spawnEnemyOn = true;
+   [SerializeField]
+   private bool spawnEnemyOn = true;
 
-    [SerializeField]
-    private bool inWave;
-    private int waveNumber = 1;
+   [SerializeField]
+   private bool inWave;
+   private int waveNumber = 1;
 
-    // timer
-    public float spawnDelay = 3.0f;
-    public float spawnTimer = 0.0f;
-    public float waveDelay = 0.0f;
-    public float waveTimer = 5.0f;
+   // timer
+   public float spawnDelay = 3.0f;
+   public float spawnTimer = 0.0f;
+   public float waveDelay = 0.0f;
+   public float waveTimer = 5.0f;
 
-    private void Awake()
-    {
-        allChildren = spawnPointParent.GetComponentsInChildren<Transform>();
-    }
+   private void Awake()
+   {
+      allChildren = spawnPointParent.GetComponentsInChildren<Transform>();
+   }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-      
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
 
-    private void Update()
-    {
-        DestroyEnemyCount();
-        spawnTimer += Time.deltaTime;
+   }
 
-        if (spawnTimer >= spawnDelay)
-        {
-            SpawnEnemy();
-            spawnTimer = 0.0f;
-        }
+   private void Update()
+   {
+      DestroyEnemyCount();
+      spawnTimer += Time.deltaTime;
 
-        StartNewWave();
-    }
+      if (spawnTimer >= spawnDelay)
+      {
+         SpawnEnemy();
+         spawnTimer = 0.0f;
+      }
 
-    private void SpawnEnemy()
-    {
-        if (spawnEnemyOn)
-        {
-            randomSpawnPointNum = Random.Range(0, allChildren.Length);
-            randomEnemyType = Random.Range(0, enemyTypeMaxRange);
+      StartNewWave();
+   }
 
-            // Spawning enemies in random locations
-            Instantiate(enemyTypes[randomEnemyType], allChildren[randomSpawnPointNum].transform.position, Quaternion.identity);
-            enemyAmount++;
-            Debug.Log("Number of enemies: " + enemyAmount);
-            if (enemyAmount == enemyAmountMax)
-            {
-                spawnEnemyOn = false;
-            }
+   private void SpawnEnemy()
+   {
+      if (spawnEnemyOn)
+      {
+         randomSpawnPointNum = Random.Range(0, allChildren.Length);
+         randomEnemyType = Random.Range(0, enemyTypeMaxRange);
 
-        }
+         // Spawning enemies in random locations
+         Instantiate(enemyTypes[randomEnemyType], allChildren[randomSpawnPointNum].transform.position, Quaternion.identity);
+         enemyAmount++;
+         Debug.Log("Number of enemies: " + enemyAmount);
+         if (enemyAmount == enemyAmountMax)
+         {
+            spawnEnemyOn = false;
+         }
 
-    }
+      }
 
-    private void DestroyEnemyCount ()
-    {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            enemyAmount = 0;
-            enemiesDead = enemyAmountMax;
-            Debug.Log("Enemy amount = " + enemyAmount);
-        }
-    }
+   }
 
-    private void StartNewWave()
-    {
-        if (enemiesDead == enemyAmountMax)
-        {
-            waveTimer -= Time.deltaTime;
-            Debug.Log("Wave Delay countdown = " + waveTimer);
+   private void DestroyEnemyCount()
+   {
+      if (Input.GetKeyDown(KeyCode.X))
+      {
+         enemyAmount = 0;
+         enemiesDead = enemyAmountMax;
+         Debug.Log("Enemy amount = " + enemyAmount);
+      }
+   }
 
-            if (waveTimer <= waveDelay)
-            {
-                WaveSpawn();
-            }
-        }
-    }
+   private void StartNewWave()
+   {
+      if (enemiesDead == enemyAmountMax)
+      {
+         waveTimer -= Time.deltaTime;
+         Debug.Log("Wave Delay countdown = " + waveTimer);
 
-    private void WaveSpawn()
-    { 
+         if (waveTimer <= waveDelay)
+         {
+            WaveSpawn();
+            waveTimer = 5.0f;
+         }
+      }
+   }
 
-            enemyAmountMax *= 2;            
-            waveNumber++;
-            spawnEnemyOn = true;
-            //Debug.Log("Spawning wave " + waveNumber);
-    }
+   private void WaveSpawn()
+   {
+
+      enemyAmountMax *= 2;
+      waveNumber++;
+      spawnEnemyOn = true;
+      //Debug.Log("Spawning wave " + waveNumber);
+   }
 
 }
