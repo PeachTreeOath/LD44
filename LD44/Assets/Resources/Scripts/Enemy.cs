5a31632f8d641f966a7203b721da2de6bf84a162
Unknown <rfonger@gmail.com>
2019-04-27 14:01:44 -0600

    Enemies move to gold

--- LD44/Assets/Resources/Scripts/Enemy.cs
+++ LD44/Assets/Resources/Scripts/Enemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    private GameObject target;
+    private float Range = 10f;
+    public float Speed = 10f;
+
+
+    // Use this for initialization
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("gold_pile");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Range = Vector2.Distance(transform.position, target.transform.position);
+        Vector2 velocity = new Vector2((transform.position.x - target.transform.position.x) * Speed, (transform.position.y - target.transform.position.y) * Speed);
+        GetComponent<Rigidbody2D>().velocity = -velocity;
     }
+
+
 }
