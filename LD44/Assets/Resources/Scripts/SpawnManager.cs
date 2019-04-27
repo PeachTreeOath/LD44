24ef958bbe3eb011a1a30b71bc1d93217f98fe43
Randy <wiafe23@gmail.com>
2019-04-27 16:59:12 -0600

    getting waves to work

--- LD44/Assets/Resources/Scripts/SpawnManager.cs
+++ LD44/Assets/Resources/Scripts/SpawnManager.cs
@@ -41,10 +41,9 @@ public class SpawnManager : MonoBehaviour
         inWave = true;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         DestroyEnemyCount();
-        WaveSpawn();
         timer += Time.deltaTime;
 
         if (timer >= spawnDelay)
@@ -77,21 +76,17 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.X))
         {
             enemyAmount = 0;
-            inWave = false;
-            //Debug.Log("Enemy amount = " + enemyAmount);
+            WaveSpawn();
+            Debug.Log("Enemy amount = " + enemyAmount);
         }
     }
 
     private void WaveSpawn()
-    {
-        if (!inWave)
-        {
-            
+    { 
             enemyAmountMax *= 2;
             waveNumber++;
             spawnEnemyOn = true;
-            Debug.Log("Spawning wave " + waveNumber);
-        }
+            //Debug.Log("Spawning wave " + waveNumber);
     }
 
 }
