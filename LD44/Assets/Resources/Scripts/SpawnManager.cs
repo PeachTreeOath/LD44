10b324ba70c0f3fc3df991baafa1dbe7ff685193
Randy <wiafe23@gmail.com>
2019-04-27 14:53:08 -0600

    Spawn point prefabs and spawnmanager changes

--- LD44/Assets/Resources/Scripts/SpawnManager.cs
+++ LD44/Assets/Resources/Scripts/SpawnManager.cs
@@ -4,86 +4,88 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-   [SerializeField]
-   private GameObject[] spawnPoints;
+    [SerializeField]
+    private GameObject[] spawnPoints;
 
-   [SerializeField]
-   private GameObject[] enemyTypes;
+    [SerializeField]
+    private GameObject[] enemyTypes;
 
-   [SerializeField]
-   private float spawnWaitTime = 8.0f;
+    [SerializeField]
+    private float spawnWaitTime = 8.0f;
 
-   [SerializeField]
-   private float waveWaitTime = 20.0f;
+    [SerializeField]
+    private float waveWaitTime = 20.0f;
 
-   [SerializeField]
-   private int enemyAmount = 2;
-   private int enemyTypeMaxRange = 0;
+    [SerializeField]
+    private int enemyAmount = 1;
+    private int enemyTypeMaxRange = 0;
 
-   private int randomSpawnPointNum;
-   private int randomEnemyType;
-   [SerializeField]
-   private bool spawnEnemyOn = true;
-   [SerializeField]
-   private bool waveSpawnOn = true;
-   private int waveNumber = 1;
+    private int randomSpawnPointNum;
+    private int randomEnemyType;
+    [SerializeField]
+    private bool spawnEnemyOn = true;
+    [SerializeField]
+    private bool waveSpawnOn = true;
+    private int waveNumber = 1;
 
-   // Start is called before the first frame update
-   void Start()
-   {
-      
-   }
+    // Start is called before the first frame update
+    void Start()
+    {
 
-   private void Update()
-   {
-      WaveSpawn(); 
-   }
+    }
 
-   private void SpawnEnemy()
-   {
-      if (spawnEnemyOn)
-      {
-         randomSpawnPointNum = Random.Range(0, spawnPoints.Length);
-         randomEnemyType = Random.Range(0, enemyTypeMaxRange);
-         for (int i = 0; i < enemyAmount; i++)
-         {
+    private void Update()
+    {
+        WaveSpawn();
+    }
+
+    private void SpawnEnemy()
+    {
+        if (spawnEnemyOn)
+        {
+            randomSpawnPointNum = Random.Range(0, spawnPoints.Length);
+            randomEnemyType = Random.Range(0, enemyTypeMaxRange);
+            //for (int i = 0; i < enemyAmount; i++)
+            //{
+            //   Instantiate(enemyTypes[randomEnemyType], spawnPoints[randomSpawnPointNum].transform.position, Quaternion.identity);
+            //}
             Instantiate(enemyTypes[randomEnemyType], spawnPoints[randomSpawnPointNum].transform.position, Quaternion.identity);
-         }
-         spawnEnemyOn = false;
-         StartCoroutine(SpawnPowerUpRoutine());
-      }
+            spawnEnemyOn = false;
+            //StartCoroutine(SpawnPowerUpRoutine());
+        }
 
-   }
+    }
 
-   private void WaveSpawn()
-   {
-      if (waveSpawnOn)
-      {
-         SpawnEnemy();
-      }
+    private void WaveSpawn()
+    {
+        if (waveSpawnOn)
+        {
+            SpawnEnemy();
+            enemyAmount++;
+        }
 
-      if (enemyAmount == 0)
-      {
-         waveSpawnOn = false;
-         StartCoroutine(WavePowerUpRoutine());
-      }
+        if (enemyAmount == 2)
+        {
+            waveSpawnOn = false;
+            StartCoroutine(WavePowerUpRoutine());
+        }
 
-      if (!waveSpawnOn)
-      {
-         waveNumber++;
-      }
-   }
+        if (!waveSpawnOn)
+        {
+            waveNumber++;
+        }
+    }
 
-   private IEnumerator SpawnPowerUpRoutine()
-   {
-      yield return new WaitForSeconds(spawnWaitTime);
-      spawnEnemyOn = true;
-   }
+    private IEnumerator SpawnPowerUpRoutine()
+    {
+        yield return new WaitForSeconds(spawnWaitTime);
+        spawnEnemyOn = true;
+    }
 
-   private IEnumerator WavePowerUpRoutine()
-   {
-      yield return new WaitForSeconds(waveWaitTime);
-      waveSpawnOn = true;
-   }
+    private IEnumerator WavePowerUpRoutine()
+    {
+        yield return new WaitForSeconds(waveWaitTime);
+        waveSpawnOn = true;
+    }
 
 }
