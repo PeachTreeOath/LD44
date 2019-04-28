6e509790530b0a7ca3b951ece795e3f32c466c40
Calvin Pinsuwan <calvin@fizzpow.com>
2019-04-28 11:58:18 -0600

    Start wave fading text

--- LD44/Assets/Resources/Scripts/UITextManager.cs
+++ LD44/Assets/Resources/Scripts/UITextManager.cs
@@ -7,15 +7,36 @@ public class UITextManager : Singleton<UITextManager>
 {
 
     public TextMeshProUGUI waveText;
+    public TextMeshProUGUI startWaveText;
     public TextMeshProUGUI enemiesLeftText;
+
+    private float totalAnimationTime = 2;
 
     public void SetWave(int wave)
     {
         waveText.text = "Wave " + wave + " of 10";
+        DoStartWaveAnimation(wave);
     }
 
     public void SetEnemiesLeft(int wave)
     {
         enemiesLeftText.text = wave + " enemies left";
+    }
+
+    public void DoStartWaveAnimation(int wave)
+    {
+        startWaveText.text = "-Wave " + wave + "-";
+        StartCoroutine("StartWaveTextCR");
+    }
+
+    IEnumerator StartWaveTextCR()
+    {
+        float timeToAnimate = 0;
+        while (timeToAnimate < totalAnimationTime)
+        {
+            timeToAnimate += Time.deltaTime;
+            startWaveText.color = new Color(startWaveText.color.r, startWaveText.color.g, startWaveText.color.b, totalAnimationTime - timeToAnimate);
+            yield return null;
+        }
     }
 }
