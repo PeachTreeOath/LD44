038ec349e6474630c1395fe01d2ffeac58e77a63
Ian <russmoonbase@gmail.com>
2019-04-28 13:37:36 -0600

    Camera shake changes

--- LD44/Assets/Resources/Scripts/CameraShake.cs
+++ LD44/Assets/Resources/Scripts/CameraShake.cs
@@ -4,94 +4,92 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    // camera offset variables
-    [SerializeField]
-    private float shake;
+   // camera offset variables
+   [SerializeField]
+   private float shake;
 
-    private float angleMax = 1.5f;
-    private float offsetMax = 0.3f;
-    private float xOffset;
-    private float yOffset;
-    private float angleOffset;
-    private float randomNegOneToOne;
+   private float angle = 1.5f;
+   private float offset = 0.3f;
+   private float xOffset;
+   private float yOffset;
+   private float angleOffset;
+   private float randomNegOneToOne;
+   private float test = 0.0f;
 
-    // turn shake on variable
-    public bool shakeOn;
+   // turn shake on variable
+   public bool shakeOn;
 
-    public float trauma;
+   public float trauma;
 
-    Vector3 originalPosition;
-    Quaternion originalRotation;
+   Vector3 originalPosition;
+   Quaternion originalRotation;
 
-    private void Awake()
-    {
-        originalPosition = transform.localPosition;
-        originalRotation = transform.localRotation;
-        shakeOn = false;
-        trauma = 0.0f;
-    }
+   private void Awake()
+   {
+      originalPosition = transform.localPosition;
+      originalRotation = transform.localRotation;
+      shakeOn = false;
+      trauma = 0.0f;
+   }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+   // Start is called before the first frame update
+   void Start()
+   {
 
-    }
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        TurnShakeOnOff();
+   // Update is called once per frame
+   void Update()
+   {
+      TurnShakeOnOff();
 
-        //if (shakeOn)
-        //{
-        //   Shake();
-        //}
+      Shake();
+   }
 
-        Shake();
-    }
+   private void Shake()
+   {
 
-    private void Shake()
-    {
+      if (trauma > 0)
+      {
 
-        if (trauma > 0)
-        {
+         shake = Mathf.Pow(trauma, 2);
+         if (shake > 1)
+         {
+            shake = 1.0f;
+         }
+         Debug.Log("Shake: " + shake);
+         //angleOffset = angle * shake * Random.Range(-1, 1);
+         //xOffset = offset * shake * Random.Range(-1, 1);
+         //yOffset = offset * shake * Random.Range(-1, 1);
+         angleOffset = angle * shake * Mathf.PerlinNoise(test - 1, test + 1) * Random.Range(-1, 1);
+         xOffset = offset * shake * Mathf.PerlinNoise(test - 2f, test + 2f) * Random.Range(-1, 1);
+         yOffset = offset * shake * Mathf.PerlinNoise(test - 3f, test + 3f) * Random.Range(-1, 1);
 
-            shake = Mathf.Pow(trauma, 2);
-            if (shake > 1)
-            {
-                shake = 1.0f;
-            }
-            Debug.Log("Shake: " + shake);
-            angleOffset = angleMax * shake * Random.Range(-1, 1);
-            xOffset = offsetMax * shake * Random.Range(-1, 1);
-            yOffset = offsetMax * shake * Random.Range(-1, 1);
+         transform.localPosition = new Vector3(xOffset, yOffset, originalPosition.z);
+         transform.localRotation = Quaternion.Euler(originalRotation.x, originalRotation.y, angleOffset);
 
-            transform.localPosition = new Vector3(xOffset, yOffset, originalPosition.z);
-            transform.localRotation = Quaternion.Euler(originalRotation.x, originalRotation.y, angleOffset);
+         trauma -= Time.deltaTime;
+      }
+      else
+      {
+         trauma = 0.0f;
+         transform.localPosition = originalPosition;
+         transform.localRotation = originalRotation;
+      }
+   }
 
-            trauma -= Time.deltaTime;
-        }
-        else
-        {
-            //shakeOn = false;
-            trauma = 0.0f;
-            transform.localPosition = originalPosition;
-            transform.localRotation = originalRotation;
-        }
-    }
+   private void TurnShakeOnOff()
+   {
+      if (Input.GetKeyDown(KeyCode.C))
+      {
+         trauma += 1.0f;
+      }
 
-    private void TurnShakeOnOff()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            shakeOn = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            trauma += 0.3f;
-        }
-    }
+      if (Input.GetKeyDown(KeyCode.V))
+      {
+         trauma += 0.3f;
+      }
+   }
 
 
 }
